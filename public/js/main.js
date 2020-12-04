e3bae4cea8e8e7ef63d74fb65cc28a64bcc5a2b4
ELPanaJose <69026987+ELPanaJose@users.noreply.github.com>
2020-12-03 20:22:00 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,24 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
+const chatForm = document.getElementById("chat-form"),
+  chatMessages = document.querySelector(".chat-messages"),
+  roomName = document.getElementById("room-name"),
+  userList = document.getElementById("users");
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true
-});
-
-const socket = io();
-
+    ignoreQueryPrefix: true
+  }),
+  socket = io();
 // Join chatroom
-socket.emit('joinRoom', { username, room });
+socket.emit("joinRoom", { username, room });
 
 // Get room and users
-socket.on('roomUsers', ({ room, users }) => {
+socket.on("roomUsers", ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
 
 // Message from server
-socket.on('message', message => {
+socket.on("message", message => {
   console.log(message);
   outputMessage(message);
 
@@ -29,42 +27,43 @@ socket.on('message', message => {
 });
 
 // Message submit
-chatForm.addEventListener('submit', e => {
+chatForm.addEventListener("submit", e => {
   e.preventDefault();
 
   // Get message text
   let msg = e.target.elements.msg.value;
-  
+
   msg = msg.trim();
-  
-  if (!msg){
+
+  if (!msg) {
     return false;
   }
 
   // Emit message to server
-  socket.emit('chatMessage', msg);
+  socket.emit("chatMessage", msg);
 
   // Clear input
-  e.target.elements.msg.value = '';
+  e.target.elements.msg.value = "";
   e.target.elements.msg.focus();
 });
 
 // Output message to DOM
 function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  const p = document.createElement('p');
-  p.classList.add('meta');
+  const div = document.createElement("div");
+  div.classList.add("message");
+  const p = document.createElement("p");
+  p.classList.add("meta");
   p.innerText = message.username;
-  p.innerHTML += `<span>${message.time}</span>`;
+  p.innerHTML += `<span> ${message.time} </span>`;
   div.appendChild(p);
-  const para = document.createElement('p');
-  para.classList.add('text');
+  const para = document.createElement("p");
+  para.classList.add("text");
   para.innerText = message.text;
   div.appendChild(para);
-  document.querySelector('.chat-messages').appendChild(div);
-  console.log("porfavor este es una prueba, no se bloquear el DOM o por ip no hagas la payasada que hace piton")
-  console.log('chat pai 🤣😢😭🤢🥳💭👻👻😂🙏😂🤫')
+  document.querySelector(".chat-messages").appendChild(div);
+  //---------> esto es la parte de advertencia
+  console.log("bloqueamos por ip , y pronto bloquearemos el DOM");
+  console.log("chat pai 🤣😢😭🤢🥳💭👻👻😂🙏😂🤫");
 }
 
 // Add room name to DOM
@@ -74,10 +73,48 @@ function outputRoomName(room) {
 
 // Add users to DOM
 function outputUsers(users) {
-  userList.innerHTML = '';
-  users.forEach(user=>{
-    const li = document.createElement('li');
+  userList.innerHTML = "";
+  users.forEach(user => {
+    const li = document.createElement("li");
     li.innerText = user.username;
     userList.appendChild(li);
   });
- }
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style("z-index", "-1");
+}
+const color = i => noise(i, float(frameCount) / 255) * 255;
+function draw() {
+  noFill();
+  beginShape();
+  for (let i = 0; i < width; i++) {
+    let m = noise(0.01 * i, float(frameCount) / 500) * height;
+    let r = color(i),
+      g = color(i),
+      b = color(i);
+    stroke(r, 0, 0, 25);
+    curveVertex(i, m);
+  }
+  endShape();
+  beginShape();
+  for (let i = 0; i < width; i++) {
+    let m = noise(0.01 * i, float(frameCount) / 700) * height;
+    let r = color(i);
+    stroke(0, 0, r, 25);
+    curveVertex(i, m);
+  }
+  endShape();
+  beginShape();
+  for (let i = 0; i < width; i++) {
+    let m = noise(0.01 * i, float(frameCount) / 900) * height;
+
+    stroke(0);
+    curveVertex(i, m);
+  }
+  endShape();
+}
